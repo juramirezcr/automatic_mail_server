a815dac725b1a50f4f7c34dc5d6e4ee8fe772db2
juramirezcr <juramirezcr@gmail.com>
2023-03-27 07:23:27 -0600

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Autenticación de SMTP',
+    'version': '12.0.1.0.0',
+    'summary': 'Envío por autenticación del SMPS',
+    'description':"""
+        Si tiene este error: SMTPServerDisconnected
+    """,
+    'category': 'Productivity/Discuss',
+    'author': 'Julio Ramírez Ramírez',
+    'website': 'https://facturaodoocr.com',
+    'license': 'LGPL-3',
+    'depends': ['mail'],
+    'installable': True,
+    'auto_install': False,
+    'images': ['static/description/banner.gif']
+}
